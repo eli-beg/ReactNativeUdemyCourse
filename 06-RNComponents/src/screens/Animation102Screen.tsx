1c3d62c86641450433e52d481dd634200b11e19e
eli-beg <begaeliana@gmail.com>
2023-04-05 11:08:08 -0300

    add AnimateValueXY in Animation102Screen

--- 06-RNComponents/src/screens/Animation102Screen.tsx
+++ 06-RNComponents/src/screens/Animation102Screen.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useRef} from 'react';
+import {View, StyleSheet, Animated, PanResponder} from 'react-native';
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: true}, // Back to zero
+      ).start();
+    },
+  });
+
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.greenBox} />
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={{
+          ...styles.greenBox,
+          transform: [{translateY: pan.y}, {translateX: pan.x}],
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
   greenBox: {
-    backgroundColor: 'green',
+    backgroundColor: '#89d7c2',
+    borderRadius: 100,
     width: 150,
     height: 150,
   },
