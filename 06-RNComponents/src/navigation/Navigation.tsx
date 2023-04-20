efa65bc594f028a0aea8e0b599f698609a1f0241
eli-beg <begaeliana@gmail.com>
2023-04-20 12:19:49 -0300

    Add InfinityScrollScreen

--- 06-RNComponents/src/navigation/Navigation.tsx
+++ 06-RNComponents/src/navigation/Navigation.tsx
@@ -9,6 +9,7 @@ import {TextInputScreen} from '../screens/TextInputScreen';
 import {PullToRefreshScreen} from '../screens/PullToRefreshScreen';
 import {SectionListScreen} from '../screens/SectionListScreen';
 import {ModalScreen} from '../screens/ModalScreen';
+import {InfiniteScrollScreen} from '../screens/InfiniteScrollScreen';
 
 const Stack = createStackNavigator();
 
@@ -27,6 +28,10 @@ const Navigation = () => {
       />
       <Stack.Screen name="SectionListScreen" component={SectionListScreen} />
       <Stack.Screen name="ModalScreen" component={ModalScreen} />
+      <Stack.Screen
+        name="InfiniteScrollScreen"
+        component={InfiniteScrollScreen}
+      />
     </Stack.Navigator>
   );
 };
---
+++ 06-RNComponents/src/screens/InfiniteScrollScreen.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+
+export const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray: number[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      newArray[i] = numbers.length + i;
+    }
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
+  };
+
+  const renderItem = (item: number) => {
+    return (
+      <Image
+        source={{uri: `https://picsum.photos/id/${item}/500/400`}}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
+    );
+  };
+  return (
+    <View>
+      <FlatList
+        data={numbers}
+        keyExtractor={item => item.toString()}
+        renderItem={({item}) => renderItem(item)}
+        ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.5} // a partir de la mitad empieza a cargar mas datos
+        ListFooterComponent={() => (
+          <View style={styles.activityIndicator}>
+            <ActivityIndicator size={30} color="pink" />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textItem: {
+    backgroundColor: 'green',
+    height: 150,
+  },
+  activityIndicator: {
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+});
