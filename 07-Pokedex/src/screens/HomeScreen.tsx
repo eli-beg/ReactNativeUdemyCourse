16e33e7c93dd62b01b4c859f54b293b8317e2df1
eli-beg <begaeliana@gmail.com>
2023-04-25 15:14:12 -0300

    add title and background image in home screen

--- 07-Pokedex/src/screens/HomeScreen.tsx
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {Text, View} from 'react-native';
+import {Image, Text} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <View>
-      <Text>HOME SCREEN</Text>
-      <Icon name="analytics-outline" size={20} />
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text>
+    </>
   );
 };
---
+++ 07-Pokedex/src/theme/appTheme.tsx
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  pokebolaBG: {
+    opacity: 0.2,
+    position: 'absolute',
+    top: -100,
+    right: -100,
+    height: 300,
+    width: 300,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
