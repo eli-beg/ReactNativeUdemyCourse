cb6a8769f485fa524b3fd6d2dcacf2ffaabadcf7
eli-beg <begaeliana@gmail.com>
2023-04-25 14:51:29 -0300

    add stack navigator and icons configuration

---
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Text, View} from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HOME SCREEN</Text>
+      <Icon name="analytics-outline" size={20} />
+    </View>
+  );
+};
---
+++ 07-Pokedex/src/screens/PokemonScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const PokemonScreen = () => {
+  return (
+    <View>
+      <Text>POKEMON SCREEN</Text>
+    </View>
+  );
+};
