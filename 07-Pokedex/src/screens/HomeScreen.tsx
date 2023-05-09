745c7a7f55b64226f8a13858e16dfa01dc814423
eli-beg <begaeliana@gmail.com>
2023-05-09 16:00:06 -0300

    add PokemonCard part 1

--- 07-Pokedex/src/screens/HomeScreen.tsx
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Image, Text} from 'react-native';
+import {Image, FlatList, Text, ActivityIndicator} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
+import {PokemonCard} from '../components/PokemonCard';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
-  const {simplePokemonList} = usePokemonPaginated();
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated();
   console.log(simplePokemonList);
   return (
     <>
@@ -14,9 +15,24 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
-        Pokedex
-      </Text>
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+        //header component
+        ListHeaderComponent={
+          <Text style={{...styles.title, ...styles.globalMargin}}>Pokedex</Text>
+        }
+        //infinitescroll
+
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator style={{height: 100}} size={30} color="grey" />
+        }
+      />
     </>
   );
 };
