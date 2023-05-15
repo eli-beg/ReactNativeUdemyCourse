d37dcb4b5a9e664382b953452563fdacf7938a1e
eli-beg <begaeliana@gmail.com>
2023-05-15 15:49:08 -0300

    pokemon screen

--- 07-Pokedex/src/screens/PokemonScreen.tsx
+++ 07-Pokedex/src/screens/PokemonScreen.tsx
@@ -1,18 +1,67 @@
 import React from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
-import {Text, View} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Text, Image} from 'react-native';
 import {RootStackParams} from '../navigation/Navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
 
 export const PokemonScreen = ({navigation, route}: Props) => {
   const {simplePokemon, color} = route.params;
+  const {name, id, picture} = simplePokemon;
 
   return (
     <View>
-      <Text>
-        {simplePokemon.name} - {color}
-      </Text>
+      <View style={{...styles.headerContainer, backgroundColor: color}}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.iconContainer}
+          onPress={() => navigation.pop()}>
+          <Icon name="arrow-back-outline" color="white" size={35} />
+        </TouchableOpacity>
+        <Text style={styles.pokemonName}>
+          {name + '\n'} #{id}
+        </Text>
+        <Image
+          source={require('../assets/pokebola-blanca.png')}
+          style={styles.pokebola}
+        />
+        <Image source={{uri: picture}} style={styles.pokemonImage} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  iconContainer: {
+    position: 'absolute',
+    left: 20,
+    top: 20,
+  },
+  pokemonName: {
+    color: 'white',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20,
+    top: 50,
+  },
+  pokebola: {
+    width: 250,
+    height: 250,
+    bottom: -20,
+    opacity: 0.7,
+  },
+  pokemonImage: {
+    width: 250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15,
+  },
+});
