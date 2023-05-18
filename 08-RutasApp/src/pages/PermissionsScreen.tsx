92bc3db45f8625bcdf4fff65e7c4aa7dedd55a68
eli-beg <begaeliana@gmail.com>
2023-05-18 17:06:52 -0300

    ask permissions

--- 08-RutasApp/src/pages/PermissionsScreen.tsx
+++ 08-RutasApp/src/pages/PermissionsScreen.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Button, Platform} from 'react-native';
+import {request, PERMISSIONS, PermissionStatus} from 'react-native-permissions';
 
 export const PermissionsScreen = () => {
+  const checkLocationPermission = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === 'ios') {
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+    }
+
+    console.log(permissionStatus);
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>PERMISSIONS SCREEN</Text>
+
+      <Button title="Permission" onPress={checkLocationPermission} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
